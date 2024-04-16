f42f8567a2279d760f4474fc1a73e83e82da98b8
Lucifer <vivekrai3944@gmail.com>
2024-04-16 19:26:03 +0530

    update

--- Jquery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('.firsth1').animate({ top: 0 }, 2000); 
-    $('.firstp1').animate({ left: 0 }, 2000);
-  });
- 
--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-// Get the slider container element
-const slider = document.querySelector('.slider');
-
-// Function to activate the slider on button click
-function activateSlider(e) {
-  // Get all elements with the class 'item' inside the slider
-  const items = document.querySelectorAll('.item');
-
-  // Move to the next item when the element with class 'next' is clicked
-  if (e.target.matches('.next')) {
-    // Append the first item to the end of the slider
-    slider.append(items[0]);
-  }
-
-  // Move to the previous item when the element with class 'prev' is clicked
-  if (e.target.matches('.prev')) {
-    // Prepend the last item to the beginning of the slider
-    slider.prepend(items[items.length - 1]);
-  }
-}
-
-// Add a click event listener to the entire document
-document.addEventListener('click', activateSlider, false);
-
-// SECTION-2///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-gsap.registerPlugin(ScrollTrigger);
-
-const textElements = gsap.utils.toArray('.text');
-
-textElements.forEach(text => {
-  gsap.to(text, {
-    backgroundSize: '100%',
-    ease: 'none',
-    scrollTrigger: {
-      trigger: text,
-      start: 'center 80%',
-      end: 'center 20%',
-      scrub: true,
-    },
-  });
+/////////// jQuery/////////////////////////
+$(document).ready(function(){
+  $('.firsth1').animate({ top: 0 }, 2000); 
+  $('.firstp1').animate({ left: 0 }, 2000);
 });
+/////////// End/////////////////////////
+
+
+// let body3=document.body.childNodes[7].firstElementChild;
+let body3first=document.getElementById("body3first")
+
+
+body3.addEventListener("click",()=>{
+  body3first.style.backgroundColor="black"
+  body3first.style.color="white"
+})
+body3first.addEventListener("dblclick",()=>{
+  body3first.style.backgroundColor="#ee9652ad"
+  body3first.style.color="black"
+})
+
+
